686bb0e128ced7f510f2d33856b1f7495500d303
Diarmid Campbell <dac70@cam.ac.uk>
2015-10-30 14:38:07 +0000

    new gitignore file

---
+++ gitRemoveIgnore.sh
@@ -0,0 +1,33 @@
+#=============== 
+#Unity generated
+#===============
+git rm -r Temp
+git rm -r Library
+git rm -r Executables
+
+
+#=====================================
+#Visual Studio / MonoDevelop generated
+#=====================================
+git rm -r ExportedObjrecur
+git rm -r obj
+git rm -r *.svd
+git rm -r *.userprefs
+git rm -r *.csproj
+git rm -r *.pidb
+git rm -r *.suo
+git rm -r *.sln
+git rm -r *.user
+git rm -r *.unityproj
+git rm -r *.booproj
+
+#============
+#OS generated
+#============
+git rm -r .DS_Store
+git rm -r .DS_Store?
+git rm -r ._*
+git rm -r .Spotlight-V100
+git rm -r .Trashes
+git rm -r ehthumbs.db
+git rm -r Thumbs.db
